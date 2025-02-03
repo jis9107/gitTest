03a7307f8032dbedcd20206007de3c8243678ad0
insooJeon <jis9107@naver.com>
2025-02-03 21:01:47 +0900

    충돌 테스트

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int num = 30;
     }
 
     // Update is called once per frame
